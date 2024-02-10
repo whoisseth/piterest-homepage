8e3163f53ac0362feac688bd9682770dc9f4c183
whoisseth <utkarshseth2019@gmail.com>
2024-02-10 15:38:58 +0530

    updated logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,12 +2,14 @@
 
 import Image from "next/image";
 import React from "react";
+import logo from "@/assets/pinterest_PNG45.png";
 
 type Props = {};
 
 export default function Logo({}: Props) {
-  const imgSrc =
-    "https://th.bing.com/th/id/R.0dea4ad3030467e2f65cde00935ba62b?rik=5H3iHU%2bDuMG8Tw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fpinterest%2fpinterest_PNG45.png&ehk=2PCNO5m5fgzmgPIIwDUJ8K%2bZmapApLojXwLeHId8n80%3d&risl=&pid=ImgRaw&r=0";
+  // const imgSrc =
+  //   "https://th.bing.com/th/id/R.0dea4ad3030467e2f65cde00935ba62b?rik=5H3iHU%2bDuMG8Tw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fpinterest%2fpinterest_PNG45.png&ehk=2PCNO5m5fgzmgPIIwDUJ8K%2bZmapApLojXwLeHId8n80%3d&risl=&pid=ImgRaw&r=0";
+  const imgSrc = logo;
 
   return (
     <div>
